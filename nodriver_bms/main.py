4eb9a1f6a95dd4809b2be0e56944fcfad95a4556
tsx0314 <tian.shixing2001@gmail.com>
2024-08-04 23:20:02 +0800

    no driver for bms booking

---
+++ nodriver_bms/main.py
@@ -0,0 +1,31 @@
+import nodriver as uc
+import asyncio
+import time
+from nodriver_browser_action import visit_target_website, check_captcha, check_queue, scroll_and_accept, fill_in_ticket_number, choose_section
+import json
+
+filename = 'config.json'
+
+with open(filename, 'r') as file:
+    config = json.load(file)
+
+print(config)
+
+url_to_visit = config["url"]
+section_data = config["sections"]
+
+async def main():
+    browser = await uc.start()
+    page = await browser.get(url_to_visit)
+    await page.set_window_size(left=0, top=0, width=600, height=1024)
+
+    await visit_target_website(page)
+    await check_captcha(page)
+    await check_queue(page)
+    await scroll_and_accept(page)
+    await fill_in_ticket_number(page, 2)
+    await choose_section(page, section_data)
+    time.sleep(1000)
+    
+if __name__ == '__main__':
+    uc.loop().run_until_complete(main())
